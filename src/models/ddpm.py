fcec49d2280731249c6fb5f6af9c9cc603e27064
Anirudh Valiveru <anirudhv@mit.edu>
2024-01-08 00:29:47 -0800

    Added initializations for main UNet blocks

--- src/models/ddpm.py
+++ src/models/ddpm.py
@@ -18,9 +18,10 @@ class SinusoidalEmbedding(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
         self.emb_dim = emb_dim
-        self.linear1 = nn.Linear(emb_dim, emb_dim * 4)
+
+        self.linear1 = nn.Linear(emb_dim // 4, emb_dim)
         self.act = nn.SiLU()
-        self.linear2 = nn.Linear(emb_dim * 4, emb_dim * 4)
+        self.linear2 = nn.Linear(emb_dim, emb_dim)
 
     def forward(self, t: torch.Tensor):
         """
@@ -30,7 +31,7 @@ class SinusoidalEmbedding(nn.Module):
         Learn linear transformation of sinusoidal embedding of t.
         """
         assert len(t.shape) == 1
-        half_dim = self.emb_dim // 2
+        half_dim = self.emb_dim // 8
         emb = math.log(10000) / (half_dim - 1)
         emb = torch.exp(torch.arange(half_dim, device=t.device) * -emb)
         emb = t[:, None] * emb[None, :]
@@ -38,20 +39,37 @@ class SinusoidalEmbedding(nn.Module):
 
         emb = self.linear2(self.act(self.linear1(emb)))
 
-        assert emb.shape == (t.shape[0], self.emb_dim * 4)
+        assert emb.shape == (t.shape[0], self.emb_dim)
         return emb
 
+
 class Downsample(nn.Module):
-    '''
+    """
     Conv2d to downsample in half.
-    '''
-    pass
+    """
+
+    def __init__(self, num_channels):
+        self.conv = nn.Conv2d(
+            num_channels, num_channels, kernel_size=3, stride=2, padding=1
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.conv(x)
+
 
 class Upsample(nn.Module):
-    '''
-    Conv2d to upsample image 2x
-    '''
-    pass
+    """
+    Conv2d to Upsample by 2x
+    """
+
+    def __init__(self, num_channels):
+        self.conv_transpose = nn.ConvTranspose2d(
+            num_channels, num_channels, kernel_size=4, stride=2, padding=1
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.conv_transpose(x)
+
 
 class ResidualBlock(nn.Module):
     """
@@ -62,6 +80,7 @@ class ResidualBlock(nn.Module):
         self,
         in_channels: int,
         out_channels: int,
+        time_dims: int,
         num_groups=32,
         dropout_p=0.1,
         has_attn=False,
@@ -74,7 +93,7 @@ class ResidualBlock(nn.Module):
         self.dropout = nn.Dropout2d(dropout_p)
 
         # Linear Layer for sinusoidal embedding
-        self.time_dense = nn.Linear(out_channels, out_channels)
+        self.time_dense = nn.Linear(time_dims, out_channels)
 
         # Downsample feature map resolution
         self.gn2 = nn.GroupNorm(num_groups=num_groups, num_channels=out_channels)
@@ -110,23 +129,6 @@ class ResidualBlock(nn.Module):
 
         return out + residual
 
-class DownsampleBlock(nn.Module):
-    '''
-    Residual Block + Downsampling. 
-    '''
-    pass
-
-class UpsampleBlock(nn.Module):
-    '''
-    Residual Block + Upsampling. Shape Layers to allow skip connections.
-    '''
-    pass
-
-class BottleneckBlock(nn.Module):
-    '''
-    Bottleneck Block. Res + Attn + Res. All shapes stay the same.
-    '''
-    pass
 
 class DiffusionUNet(nn.Module):
     """
@@ -134,21 +136,62 @@ class DiffusionUNet(nn.Module):
     """
 
     def __init__(self, in_channels=3):
+        """
+        Initialization for a 32 x 32 dataset.
+        """
         super().__init__()
+        channels_list = [128, 128, 256, 256, 256]
+        resolutions_list = [32, 32, 16, 8, 4]
+        TIME_EMBED_NCHAN = channels_list[0] * 4
 
-        self.conv1 = nn.Conv2d(in_channels, 128)
+        self.conv1 = nn.Conv2d(in_channels, channels_list[0])
+        self.time_embed = SinusoidalEmbedding(
+            TIME_EMBED_NCHAN
+        )  # Start at 4x initial dimension. Max number of channels in this network's bottleneck.
 
-        self.time_embed = SinusoidalEmbedding()
-        
         # Encoder Layers:
+        self.enc_res1 = ResidualBlock(
+            channels_list[0], channels_list[1], TIME_EMBED_NCHAN
+        )
+        self.ds1 = Downsample(channels_list[1])  # 16
+        self.enc_res2 = ResidualBlock(
+            channels_list[1], channels_list[2], TIME_EMBED_NCHAN, has_attn=True
+        )
+        self.ds2 = Downsample(channels_list[2])  # 8
+        self.enc_res3 = ResidualBlock(
+            channels_list[2], channels_list[3], TIME_EMBED_NCHAN
+        )
+        self.ds3 = Downsample(channels_list[3])  # 4
+        self.enc_res4 = ResidualBlock(
+            channels_list[3], channels_list[4], TIME_EMBED_NCHAN
+        )
 
         # Bottleneck Layer
+        self.mid_res1 = ResidualBlock(
+            channels_list[-1], channels_list[-1], TIME_EMBED_NCHAN, has_attn=True
+        )
+        self.mid_res2 = ResidualBlock(
+            channels_list[-1], channels_list[-1], TIME_EMBED_NCHAN
+        )
 
         # Decoder Layers
-
+        self.dec_res4 = ResidualBlock(
+            channels_list[4], channels_list[3], TIME_EMBED_NCHAN
+        )
+        self.us3 = Upsample(channels_list[3])  # 8
+        self.dec_res3 = ResidualBlock(
+            channels_list[3], channels_list[2], TIME_EMBED_NCHAN
+        )
+        self.us2 = Upsample(channels_list[2])  # 16
+        self.dec_res2 = ResidualBlock(
+            channels_list[2], channels_list[1], TIME_EMBED_NCHAN, has_attn=True
+        )
+        self.us1 = Upsample(channels_list[1])  # 32
+        self.dec_res1 = ResidualBlock(
+            channels_list[1], channels_list[0], TIME_EMBED_NCHAN,
+        )
 
     def forward(self, img: torch.Tensor, t: torch.Tensor):
         # TODO: Fix Positional Embedding t. Only compute linear transform once, use the same embedding in all layers.
-        # Implement forward logic through all layers. Make sure to implement skip connections!        
+        # Implement forward logic through all layers. Make sure to implement skip connections!
         emb_t = self.time_embed(t)
-        
